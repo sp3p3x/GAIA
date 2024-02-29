9146ef297853de46a57ef4d626f44a28620e4000
sp3p3x <594b4b@gmail.com>
2024-02-29 16:41:46 +0530

    feat: added path finding and tracing code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, cv2
+import time, cv2, re
 import flet as ft
 from PIL import Image, ImageDraw, ImageFont
 
@@ -9,6 +9,150 @@ DEBUG = True
 def debug(data):
     if DEBUG:
         print(f"{time.strftime('%H:%M:%S', time.gmtime())}: {data}\n")
+
+
+class TracePath:
+
+    transparent = cv2.imread("assets/mapComp/transparent.png", cv2.IMREAD_UNCHANGED)
+    trace = cv2.imread("assets/mapComp/trace.png", cv2.IMREAD_UNCHANGED)
+
+    def genRow(self, row):
+        newRow = []
+        for block in row:
+            if block.lower() == "transparent":
+                newRow.append(TracePath.transparent)
+            if block.lower() == "trace":
+                newRow.append(TracePath.trace)
+        rowImage = cv2.hconcat(newRow)
+        return rowImage
+
+    def genCol(self, col):
+        colImage = cv2.vconcat(col)
+        return colImage
+
+    def genMap(self, mapData, mapName="map"):
+        rows = []
+        for row in mapData:
+            rows.append(self.genRow(row))
+        columns = self.genCol(rows)
+        cv2.imwrite(f"assets/genMaps/{mapName}.png", columns)
+
+    def generate(self, data):
+        MapGenerator = TracePath()
+        mapName = "map".replace(" ", "_")
+        MapGenerator.genMap(data, mapName)
+
+    def main(self, path):
+        blockSize = 300
+        mapPath = cv2.imread("assets/genMaps/ground_floor.png")
+        mapSize = [mapPath.shape[1], mapPath.shape[0]]
+
+        rows = int(mapSize[0] / 300)
+        cols = int(mapSize[1] / 300)
+        data = ((((" transparent " * rows).strip()) + "\n") * cols).strip()
+
+        mapData = []
+        rows = data.split("\n")
+        for row in rows:
+            mapData.append(row.split())
+
+        debug(path)
+        path = [[4, 3], [4, 2], [4, 1], [3, 1], [2, 1], [2, 2]]
+
+        for i in path:
+            mapData[i[1]][i[0]] = "trace"
+
+        self.generate(mapData)
+
+
+class PathFind:
+    inputStr = """empty empty empty empty empty empty empty
+    empty n102 path path path scc empty
+    empty n011 path empty path s011 empty
+    empty n001 path empty path s011 empty"""
+
+    # with open("assets/db/floor_1.txt", "r") as f:
+    #     inputStr = f.read()
+
+    pathMatrix = [i.split(" ") for i in inputStr.split("\n")]
+
+    traversedPath = []
+    pathFound = False
+
+    def getBlock(self, coords):
+        try:
+            room = self.pathMatrix[coords[1]][coords[0]]
+        except:
+            return False
+        return room
+
+    def getCoords(self, room):
+        coords = None
+        for i in range(len(self.pathMatrix)):
+            try:
+                if self.pathMatrix[i].index(room):
+                    coords = (self.pathMatrix[i].index(room), i)
+            except:
+                pass
+        return coords
+
+    def getSurrounding(self, coords):
+        x = coords[0]
+        y = coords[1]
+
+        foobar = [
+            [x, y - 1, self.getBlock([x, y - 1])],
+            [x - 1, y, self.getBlock([x - 1, y])],
+            [x + 1, y, self.getBlock([x + 1, y])],
+            [x, y + 1, self.getBlock([x, y + 1])],
+        ]
+
+        surroundCoords = []
+
+        for block in foobar:
+            if block[2] != False:
+                surroundCoords.append(block)
+
+        return surroundCoords
+
+    def recurse(self, start, end):
+        global pathFound, traversedPath
+
+        queue = []
+
+        if type(start) == str:
+            startCoords = self.getCoords(start)
+        else:
+            startCoords = start
+
+        for block in self.getSurrounding(startCoords):
+            if block[2] != end:
+                if block[2] == "path":
+                    foo = [block[0], block[1]]
+                    if foo not in self.traversedPath and not self.pathFound:
+                        self.traversedPath.append(foo)
+                        queue.append(foo)
+                        self.recurse(queue[0], end)
+                        queue.pop()
+                if "path" not in self.getSurrounding(startCoords):
+                    pass
+            else:
+                self.pathFound = True
+
+    def findPath(self, start, end):
+        global traversedPath, pathFound
+
+        self.recurse(start, end)
+
+        if self.pathFound:
+            path = self.traversedPath
+        else:
+            print("Cannot find path!")
+
+        self.traversedPath = []
+        self.pathFound = False
+
+        return path
 
 
 class GenMap:
@@ -69,10 +213,37 @@ class Animation:
 class Functions:
 
     def roomNumberIsValid(roomNum):
-        if len(roomNum) > 3:
-            return True
-        else:
-            return False
+        # if len(roomNum) > 3:
+        #     return True
+        # else:
+        #     return False
+
+        roomNum = roomNum.upper()
+
+        validpattern1 = re.compile(r"[A-Z]\d{3}")
+        validpattern2 = re.compile(r"[A-Z]\d{3}[A-Z]")
+
+        def sanitize(roomNum):
+            roomNum = roomNum.replace(" ", "").replace("-", "")
+            return roomNum
+
+        def valid(roomNum):
+            roomNum = sanitize(roomNum)
+            if validpattern1.fullmatch(roomNum) or validpattern2.fullmatch(roomNum):
+                return True
+            else:
+                return False
+
+        def valid_r(n):
+            n = sanitize(n)
+            validpattern1 = re.compile(r"[SNA][0-4][0-1][0-9]")
+            validpattern2 = re.compile(r"[SNA][0-4][0-1][0-9][A-E]")
+            if validpattern1.search(n) or validpattern2.search(n) or n == "SCC":
+                return True
+            else:
+                return False
+
+        return valid_r(roomNum)
 
 
 class Pages:
@@ -81,13 +252,20 @@ class Pages:
     toRoomNumber = None
 
     mapImage = ft.Image(
-        src="assets/genMaps/placeholderMap.png",
+        src="assets/genMaps/ground_floor.png",
+        fit=ft.ImageFit.FILL,
+    )
+
+    pathTraceImage = ft.Image(
+        src="assets/genMaps/map.png",
         fit=ft.ImageFit.FILL,
     )
 
     def updateMapSize(app, width, height):
         Pages.mapImage.width = width
         Pages.mapImage.height = height - 80
+        Pages.pathTraceImage.width = width
+        Pages.pathTraceImage.height = height - 80
 
     def homePage(app):
         return ft.View(
@@ -242,6 +420,19 @@ class Pages:
         global fromRoomNumber
         global toRoomNumber
 
+        finder = PathFind()
+        path = finder.findPath(fromRoomNumber, toRoomNumber)
+        debug(path)
+
+        tracer = TracePath()
+        tracer.main(path)
+
+        mapImage = Pages.mapImage
+        Pages.updateMapSize(app, app.page.window_width, app.page.window_height)
+
+        pathTraceImage = Pages.pathTraceImage
+        Pages.updateMapSize(app, app.page.window_width, app.page.window_height)
+
         return ft.View(
             "/pathPage",
             [
@@ -250,9 +441,22 @@ class Pages:
                     bgcolor=ft.colors.SURFACE_VARIANT,
                     center_title=True,
                 ),
-                ft.Text(fromRoomNumber),
-                ft.Text(toRoomNumber),
-                ft.ElevatedButton("Next", on_click=lambda _: app.page.go("/homePage")),
+                ft.Container(
+                    ft.Stack(
+                        [
+                            mapImage,
+                            pathTraceImage,
+                            ft.Container(
+                                content=ft.Row(
+                                    [],
+                                    alignment=ft.MainAxisAlignment.CENTER,
+                                    spacing=10,
+                                ),
+                                alignment=ft.alignment.center,
+                            ),
+                        ]
+                    ),
+                ),
             ],
         )
 
@@ -311,7 +515,7 @@ class Main:
         # self.page.window_maximized = True
         self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
         self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        # self.splash()
+        self.splash()
         self.main()
 
     def view_pop(self, view):
@@ -342,13 +546,14 @@ class Main:
         if self.page.route == "/fromInputPage":
             self.page.views.append(Pages.fromInputPage(self))
         if self.page.route == "/toInputPage":
+            self.page.views.pop()
             self.page.views.append(Pages.toInputPage(self))
         if self.page.route == "/pathPage":
+            self.page.views.pop()
             self.page.views.append(Pages.pathPage(self))
         if self.page.route == "/mapGenPage":
             self.page.views.append(Pages.mapGenPage(self))
-        for i in self.page.views:
-            debug(i.route)
+        debug(self.page.views)
         self.page.update()
 
     def pageResize(self, event):
